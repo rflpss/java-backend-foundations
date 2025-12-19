a124ad5224b222c36568ff4dbb49bac3a65adf65
rflpss <contact.rafael.lps@gmail.com>
2025-12-19 17:47:00 +0100

    Practice: Logic challenge (ASCII Rocket)

---
+++ Java-Studies/02-Java-Platform-Basics/Rocket.java
@@ -0,0 +1,15 @@
+public class Rocket {
+
+	public static void main(String[] args) {
+		
+		System.out.println("   ^   ");
+		System.out.println("  / \\  ");
+		System.out.println(" /   \\ ");
+		System.out.println("|  R  |");
+		System.out.println("|  F  |");
+		System.out.println("|  L  |");
+		System.out.println(" ----- ");
+		System.out.println("  / \\  ");
+		System.out.println(" /   \\ ");
+	}
+}
